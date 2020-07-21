30e468aa1a6e3dd7d021b91c3a55a7590e1f5e73
Dhanasekar Mohanraj <dhanasekar.mohanraj@syncfusion.com>
2020-07-21 15:28:02 +0530

    How to restrict the expand and collapse the groups when click on the caption row in WPF DataGrid (SfDataGrid)?

---
+++ WPF/ViewModel/ViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Syncfusion.Windows.Shared;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SfDataGridDemo
+{
+    public class ViewModel
+    {
+        private ObservableCollection<OrderInfo> _orders;
+        public ObservableCollection<OrderInfo> Orders
+        {
+            get { return _orders; }
+            set { _orders = value; }
+        }
+
+        public ViewModel()
+        {
+            _orders = new ObservableCollection<OrderInfo>();
+            this.GenerateOrders();
+        }
+
+        private void GenerateOrders()
+        {
+            _orders.Add(new OrderInfo() { OrderID = 1001, CustomerName = "Maria Anders", Country="Germany", CustomerID = "ALFKI", UnitPrice = 10700 });
+            _orders.Add(new OrderInfo() { OrderID = 1002, CustomerName = "Ana Trujilo", Country = "Mexico", CustomerID = "ANATR", UnitPrice = 36000 });
+            _orders.Add(new OrderInfo() { OrderID = 1003, CustomerName = "Antonio Moreno", Country = "Mexico", CustomerID = "ANTON", UnitPrice = 40040 });
+            _orders.Add(new OrderInfo() { OrderID = 1004, CustomerName = "Thomas Hardy", Country = "UK", CustomerID = "AROUT", UnitPrice=10700});
+            _orders.Add(new OrderInfo() { OrderID = 1005, CustomerName="Christina Berglund", Country = "Sweden", CustomerID = "BERGS", UnitPrice=20300 });
+            _orders.Add(new OrderInfo() { OrderID = 1006, CustomerName = "Hanna Moos", Country = "Germany", CustomerID = "BLAUS", UnitPrice = 10700 });
+            _orders.Add(new OrderInfo() { OrderID = 1007, CustomerName = "Frederique Citeaux", Country = "France", CustomerID = "BLONP", UnitPrice = 10700 });
+            _orders.Add(new OrderInfo() { OrderID = 1008, CustomerName = "Martin Sommer", Country = "Spain", CustomerID = "BOLID", UnitPrice = 35000 });
+            _orders.Add(new OrderInfo() { OrderID = 1009, CustomerName = "Laurence Lebihan", Country = "France", CustomerID = "BONAP", UnitPrice = 20030 });
+            _orders.Add(new OrderInfo() { OrderID = 1010, CustomerName = "Elizabeth Lincoln", Country = "Canada", CustomerID = "BOTTM", UnitPrice = 54000 });
+
+            _orders.Add(new OrderInfo() { OrderID = 1001, CustomerName = "Maria Anders", Country = "Germany", CustomerID = "ALFKI", UnitPrice = 10700 });
+            _orders.Add(new OrderInfo() { OrderID = 1002, CustomerName = "Ana Trujilo", Country = "Mexico", CustomerID = "ANATR", UnitPrice = 36000 });
+            _orders.Add(new OrderInfo() { OrderID = 1003, CustomerName = "Antonio Moreno", Country = "Mexico", CustomerID = "ANTON", UnitPrice = 40040 });
+            _orders.Add(new OrderInfo() { OrderID = 1004, CustomerName = "Thomas Hardy", Country = "UK", CustomerID = "AROUT", UnitPrice = 10700 });
+            _orders.Add(new OrderInfo() { OrderID = 1005, CustomerName = "Christina Berglund", Country = "Sweden", CustomerID = "BERGS", UnitPrice = 20300 });
+            _orders.Add(new OrderInfo() { OrderID = 1006, CustomerName = "Hanna Moos", Country = "Germany", CustomerID = "BLAUS", UnitPrice = 10700 });
+            _orders.Add(new OrderInfo() { OrderID = 1007, CustomerName = "Frederique Citeaux", Country = "France", CustomerID = "BLONP", UnitPrice = 10700 });
+            _orders.Add(new OrderInfo() { OrderID = 1008, CustomerName = "Martin Sommer", Country = "Spain", CustomerID = "BOLID", UnitPrice = 35000 });
+            _orders.Add(new OrderInfo() { OrderID = 1009, CustomerName = "Laurence Lebihan", Country = "France", CustomerID = "BONAP", UnitPrice = 20030 });
+            _orders.Add(new OrderInfo() { OrderID = 1010, CustomerName = "Elizabeth Lincoln", Country = "Canada", CustomerID = "BOTTM", UnitPrice = 54000 });
+
+        }
+    }
+}
+            
+        
+     
+
